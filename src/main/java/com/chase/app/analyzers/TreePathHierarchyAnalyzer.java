247748a2416140d86570b88d9408ae14bfca4c01
Nimrod Dolev <nimrodavid@gmail.com>
2021-09-29 17:17:42 +0300

    namespace fix

--- src/main/java/com/chase/app/analyzers/TreePathHierarchyAnalyzer.java
+++ src/main/java/com/chase/app/analyzers/TreePathHierarchyAnalyzer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.chaseapp.localsearch.analyzers;
+package com.chase.app.analyzers;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
